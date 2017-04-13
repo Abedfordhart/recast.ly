5584e63ba47b7735004dd10add42b8764203c7f3
Hack Reactor Student <student@hackreactor.com>
2017-04-12 18:57:05 -0700

    Pass tests for VideoList and VideoListEntry

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,11 @@ var App = () => (
   <div>
     <Nav />
     <div className="col-md-7">
-      <VideoPlayer/>
+      <VideoPlayer videos = {exampleVideoData}/>
     </div>
     <div className="col-md-5">
-      <VideoList/>
+      <VideoList videos = {exampleVideoData}/>
+    }
     </div>
   </div>
 );
